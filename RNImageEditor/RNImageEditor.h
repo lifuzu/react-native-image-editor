fca56b0e8affa433fe97cda6133d23efa9eb6a94
Richard Lee <lifuzu@gmail.com>
2015-08-26 14:30:49 -0700

    Initial commit

---
+++ RNImageEditor/RNImageEditor.h
@@ -0,0 +1,15 @@
+//
+//  RNImageEditor.h
+//  RNImageEditor
+//
+//  Created by Richard Lee on 8/26/15.
+//  Copyright (c) 2015 Carystal. All rights reserved.
+//
+
+//#import "RCTView.h"
+//#import "RCTBridge.h"
+#import "Foundation/Foundation.h"
+
+@interface RNImageEditor : NSObject
+
+@end
---
+++ RNImageEditor/RNImageEditor.m
@@ -0,0 +1,13 @@
+//
+//  RNImageEditor.m
+//  RNImageEditor
+//
+//  Created by Richard Lee on 8/26/15.
+//  Copyright (c) 2015 weimed. All rights reserved.
+//
+
+#import "RNImageEditor.h"
+
+@implementation RNImageEditor
+
+@end
